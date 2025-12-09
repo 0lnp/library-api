dfb96df575407ce219bef71dcf043b76bf55c470
Ken <0x6b656e@proton.me>
2025-12-09 14:42:00 +0700

    chore(db): add migration commands

--- src/infrastructure/databases/typeorm_data_source.ts
+++ src/infrastructure/databases/typeorm_data_source.ts
@@ -18,7 +18,11 @@ export const typeORMDataSourceOptions = (
 
 export default new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  host: process.env.DATABASE_HOST || "localhost",
+  port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+  username: process.env.DATABASE_USER || "postgres",
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME || "cinema_db",
   entities: [UserORMEntity, RefreshTokenORMEntity],
   migrations: ["migrations/*.ts"],
   synchronize: false,
